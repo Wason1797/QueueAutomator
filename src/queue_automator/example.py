c763f0335c71f69f8873dd366dee00e3341a05c7
Wason1797 <wabrborich@hotmail.com>
2021-12-29 17:04:42 -0500

    Improve example file to illustrate a more complex use case
    * Add an example with 4 worker functions and 3 processing queues

--- src/queue_automator/example.py
+++ src/queue_automator/example.py
@@ -1,22 +1,39 @@
+from queue_automator import QueueAutomator
 from time import perf_counter, sleep
-from .main import QueueAutomator
+
 
 automator = QueueAutomator()
 
 
 @automator.register_as_worker_function(output_queue_name='square_queue', process_count=2)
 def do_work(item):
-    sleep(1)
+    sleep(2)
     result = item*2
     print(f'{item} times two {result}')
     return result
 
 
-@automator.register_as_worker_function(input_queue_name='square_queue', process_count=2)
+@automator.register_as_worker_function(input_queue_name='square_queue', output_queue_name='cube_queue', process_count=2)
 def do_work_2(item):
-    sleep(1)
+    sleep(2)
     result = item**2
     print(f'{item} squared {result}')
+    return result
+
+
+@automator.register_as_worker_function(input_queue_name='cube_queue', output_queue_name='add_2_queue', process_count=2)
+def do_work_3(item):
+    sleep(2)
+    result = item**3
+    print(f'{item} cubed {result}')
+    return result
+
+
+@automator.register_as_worker_function(input_queue_name='add_2_queue', process_count=2)
+def do_work_4(item):
+    sleep(2)
+    result = item+2
+    print(f'{item} + 2 {result}')
     return result
 
 
