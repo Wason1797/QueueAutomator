8594bd2514eab11f093965ae28cb9cc04141ac76
Wladymir Brborich <wabrborich@hotmail.com>
2022-08-23 17:21:10 -0500

    improve inner library organization
    * Move the contents of main.py to automator.py so the naming was clearer
    
    * Also move the example outside the src folder of the library as it is not library code
    
    * Add support to insert data to a queue other than the initial one
    
    * Fix bug in the validate_non_empty_args helper
    
    * add a reset() api to clear the QueueAutomator state

--- examples/automator.py
+++ examples/automator.py
@@ -1,12 +1,16 @@
-from queue_automator import QueueAutomator
 from time import perf_counter, sleep
 
+from queue_automator import QueueAutomator
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 automator = QueueAutomator()
 
 
 @automator.register_as_worker_function(output_queue_name='square_queue', process_count=2)
-def do_work(item):
+def do_work(item: int) -> int:
     sleep(2)
     result = item*2
     print(f'{item} times two {result}')
@@ -14,7 +18,7 @@ def do_work(item):
 
 
 @automator.register_as_worker_function(input_queue_name='square_queue', output_queue_name='cube_queue', process_count=2)
-def do_work_2(item):
+def do_work_2(item: int) -> int:
     sleep(2)
     result = item**2
     print(f'{item} squared {result}')
@@ -22,7 +26,7 @@ def do_work_2(item):
 
 
 @automator.register_as_worker_function(input_queue_name='cube_queue', output_queue_name='add_2_queue', process_count=2)
-def do_work_3(item):
+def do_work_3(item: int) -> int:
     sleep(2)
     result = item**3
     print(f'{item} cubed {result}')
@@ -30,7 +34,7 @@ def do_work_3(item):
 
 
 @automator.register_as_worker_function(input_queue_name='add_2_queue', process_count=2)
-def do_work_4(item):
+def do_work_4(item: int) -> int:
     sleep(2)
     result = item+2
     print(f'{item} + 2 {result}')
@@ -42,7 +46,7 @@ if __name__ == '__main__':
     input_data = range(30)
     automator.set_input_data(input_data)
     results = automator.run()
-    print(results)
     end = perf_counter()
 
+    print(results)
     print(f'Took {end-start:0.2f}s')
